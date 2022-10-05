3ed93d8ae8d87f890ff8dca491b5bdab41e3aca4
limperatri <lautaro.d.gutierrez@gmail.com>
2022-10-05 15:54:30 -0300

    Initial Setup

---
+++ api/index.ts
@@ -0,0 +1,10 @@
+import 'dotenv'
+import express from 'express'
+const app = express();
+import db from './models';
+
+db.sequelize.sync().then(() => {
+    app.listen(process.env.PORT || 3001, async () => {
+        console.log(`%s Listening on port ${process.env.PORT}`)
+    })
+})
