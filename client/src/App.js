d642c3452924a67878e437b7eebfeab59002a4f6
limperatri <lautaro.d.gutierrez@gmail.com>
2022-11-08 19:00:13 -0300

    landingPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage/Landing'
+
 function App() {
   return (
-    <div className="App">
-      <p>ola</p>
-    </div>
-  );
+    <div >
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </div >
+  )
 }
-
-export default App;
+export default App
